ad2e549a2d63b2a45946480db6d939159da7f03e
Saptarshi Dey <dragonbeast.saiyan@gmail.com>
2025-03-11 14:46:32 +0530

    Create main.java

---
+++ Expression Evaluator/main.java
@@ -0,0 +1,26 @@
+import Evaluator.ExpressionEvaluator;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        ExpressionEvaluator exp = new ExpressionEvaluator(new HashMap<>(4){{
+            put("x", 3.0);
+        }});
+        String equation1 = "1.3+e^(2.3-1.2)-pi";
+        String equation2 = "1.2+(4.5-2.2/0.5+(3.45-2.22))+3.5-2.1";
+        String equation3 = "x^2-2*x+1";
+        try{
+            System.out.println("Equation-1: " + equation1);
+            System.out.println("Result: " + exp.evaluateEquation(equation1, 3, true));
+            System.out.println("\nEquation-2:" + equation2);
+            System.out.println("Result: " + exp.evaluateEquation(equation2, 3, true));
+            System.out.println("\nEquation-3:" + equation3);
+            System.out.println("Result: " + exp.evaluateEquation(equation3, 3, true));
+        } catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            //System.out.println("Invalid expression: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
