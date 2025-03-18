9ac9e6da13a7449629094b5f0c43a89d7d47a2ad
Saptarshi Dey <dragonbeast.saiyan@gmail.com>
2025-03-18 10:35:59 +0530

    Update Evaluator.java

--- Expression Evaluator/Evaluator/Evaluator.java
+++ Expression Evaluator/Evaluator/Evaluator.java
@@ -7,7 +7,7 @@ public interface Evaluator {
     
     // List of terminal symbols in decreasing order of precedence
     List<Character> terminalSymbols = new ArrayList<>(){{
-        add('^'); add('*'); add('/'); add('-'); add('+');
+        add('^'); add('/'); add('*'); add('-'); add('+');
     }};
 
     double evaluateEquation(String equation, int precision, boolean verbose) throws Exception;
