78b7868ef8c84816951be8f580828dbe639b8df5
DarkMortal <dragonbeast.saiyan@gmail.com>
2025-03-22 14:24:54 +0530

    Updated equation evaluator
    Updated the package: Complex Expression Evaluator

--- Expression Evaluator/Main.java
+++ Expression Evaluator/Main.java
@@ -1,26 +1,29 @@
-import Evaluator.ExpressionEvaluator;
+import evaluator.ExpressionEvaluator;
 import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         ExpressionEvaluator exp = new ExpressionEvaluator(new HashMap<>(4){{
+            // fifth root of unity
             put("x", 3.0);
         }});
-        String equation1 = "1.3+e^(2.3-1.2)-pi";
-        String equation2 = "1.2+(4.5-2.2/0.5+(3.45-2.22))+3.5-2.1";
-        String equation3 = "3*x^2-2*x+1";
+        String[] equations = {
+                "-1.3+e^(2.3-1.2)-pi", "3^-1+2^-2",
+                "1.2+(4.5-2.2/0.5+(3.45-2.22))+3.5-2.1", "3*x^2-2*x+1"
+        };
         try{
-            System.out.println("Equation-1: " + equation1);
-            System.out.println("Result: " + exp.evaluateEquation(equation1, 3, true));
-            System.out.println("\nEquation-2: " + equation2);
-            System.out.println("Result: " + exp.evaluateEquation(equation2, 3, true));
-            System.out.println("\nEquation-3: " + equation3 + " where x = " + exp.getVariable("x"));
-            System.out.println("Result: " + exp.evaluateEquation(equation3, 3, true));
+            for(String equation: equations){
+                if(equation.contains("x"))
+                    System.out.println("Equation: " + equation + " where x = " + exp.getVariable("x"));
+                else System.out.println("Equation: " + equation);
+                System.out.println("Result: " + exp.evaluateEquation(equation, 3, true) + "\n");
+            }
         } catch(ArithmeticException e){
             System.out.println(e.getMessage());
         } catch (Exception e) {
-            //System.out.println("Invalid expression: " + e.getMessage());
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
         }
     }
 }
+
